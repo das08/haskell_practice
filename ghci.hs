f16a28577a98f1eee40909eed9d8d99a18356684
das08 <takeda5955@gmail.com>
2020-08-26 09:11:29 +0900

    lambda and foldl

--- ghci.hs
+++ ghci.hs
@@ -10,9 +10,9 @@ factorial2 ::(Integral a) => a -> a
 factorial2 0 = 1
 factorial2 n = n*factorial2 (n-1)
 
-sum' ::(Num a) => [a] -> a
-sum' [] = 0
-sum' (x:y) = x + sum' y
+-- sum' ::(Num a) => [a] -> a
+-- sum' [] = 0
+-- sum' (x:y) = x + sum' y
 
 capital ::String -> String
 capital "" = "Empty"
@@ -89,3 +89,28 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []  
 zipWith' _ _ [] = []  
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys  
+
+filter' ::(a -> Bool) -> [a] -> [a]
+filter' _ [] =[]
+filter' f (x:xs)
+    | f x = x: filter' f xs
+    |otherwise = filter' f xs
+
+largestDivisible :: (Integral a) => a  
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3839 == 0
+
+chain ::(Integral a) => a -> [a]
+chain 1 =[1]
+chain n
+    | odd n = n: chain (n*3 +1)
+    | even n = n: chain (n `div` 2)
+
+sum' ::(Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+sum2 ::(Num a) => [a] -> a
+sum2 = foldl (+) 0
+
+elem' ::(Eq a) => a -> [a] -> Bool
+elem' x = foldl (\acc a -> if a==x then True else acc) False
