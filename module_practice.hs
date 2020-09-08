71d8beacf168e1c95a945cf5d98716ae44e04c39
das08 <takeda5955@gmail.com>
2020-09-08 10:48:48 +0900

    typeclass 1

--- module_practice.hs
+++ module_practice.hs
@@ -1,7 +1,7 @@
 import Data.List  
-import Data.Char
+import Data.Char ()
 import qualified Data.Map as Map
-import Geometry
+import Geometry ()
   
 numUniques :: (Eq a) => [a] -> Int  
 numUniques = length . nub  
