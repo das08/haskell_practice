80824ad902698ea5ceba786ac7de3cee7a7460ee
das08 <takeda5955@gmail.com>
2020-09-09 10:39:22 +0900

    3

--- typeclass.hs
+++ typeclass.hs
@@ -49,3 +49,32 @@ treeElem x (Node a left right)
     | x == a = True
     | x < a = treeElem x left
     | x > a = treeElem x right
+
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True 
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red Light"
+    show Yellow = "Yellow Light"
+    show Green = "Green Light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
