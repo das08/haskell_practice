a85949b3ce37e81e939529b38c33979bf4730bd6
das08 <takeda5955@gmail.com>
2020-09-12 21:50:02 +0900

    io2

--- io.hs
+++ io.hs
@@ -8,7 +8,10 @@
 import Control.Monad
 import Data.Char
 
-main = forever $ do
-    putStr "Enter something: "
-    line <- getLine
-    putStrLn $ map toUpper line
+main = do   
+    colors <- forM [1,2,3,4] (\a -> do  
+        putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"  
+        color <- getLine
+        return color)  
+    putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "  
+    mapM putStrLn colors  
